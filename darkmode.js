d08f1856bced5d320cadceeae037b2808d7a3f95
Melanie Hendricks <melanie.r.hendricks@gmail.com>
2023-03-27 15:19:18 -0700

    fixed darkmode toggle flashing issue

--- darkmode.js
+++ darkmode.js
@@ -3,6 +3,12 @@ export function toggleMode() {
 
   // access HTML head element
   let head = document.head;
+
+  // create lightmode css link element + add attributes
+  let lightLink = document.createElement("link");
+  lightLink.rel = "stylesheet";
+  lightLink.href = "lightmode.css";
+
   // create darkmode css link element + add attributes
   let darkLink = document.createElement("link");
   darkLink.rel = "stylesheet";
@@ -11,12 +17,16 @@ export function toggleMode() {
   // access slider
   let slider = document.querySelector(".slider");
 
+  // start in light mode
+  head.appendChild(lightLink);
   // listen for end of transition, then change mode
   slider.addEventListener("transitionend", () => {
     if (toggle.checked == true) {
+      head.removeChild(lightLink);
       head.appendChild(darkLink);
     } else {
       head.removeChild(darkLink);
+      head.appendChild(lightLink);
     }
   });
 }
