2cebb54a5ed965e2375e913d6a9ad714b91621a5
Melanie Hendricks <melanie.r.hendricks@gmail.com>
2023-04-30 17:55:51 -0700

    updated projects.html with external links

--- projects.js
+++ projects.js
@@ -44,22 +44,50 @@ export function fetchProjects() {
         // append skills div to project div
         skills.appendChild(desc);
         project.appendChild(skills);
+        // ---------------------------------------------
+        // externalLinks
+        const externalLinksDiv = document.createElement("div");
+        externalLinksDiv.setAttribute("class", "externalLinks");
 
-        // create github div + icon <a> tag
-        const githubDiv = document.createElement("div");
-        githubDiv.setAttribute("class", "githubLink");
+        // gitDiv + <a> tag
+        const gitDiv = document.createElement("div");
+        gitDiv.setAttribute("class", "gitDiv");
         const gitIcon = document.createElement("a");
 
-        // get url and set <a> attributes
-        // append gitIcon to githubDiv
-        // append githubDiv to project
-        let url = projects[i].url;
-        gitIcon.setAttribute("href", url);
+        // get gitUrl and set <a> attributes
+        // append gitIcon to gitDiv
+        // append gitDiv to externalLinks
+        let gitUrl = projects[i].gitUrl;
+        gitIcon.setAttribute("href", gitUrl);
         gitIcon.setAttribute("target", "_blank");
         gitIcon.setAttribute("class", "fa-brands fa-github fa-2x");
-        gitIcon.setAttribute("title", "check out this repo on my GitHub!");
-        githubDiv.appendChild(gitIcon);
-        project.appendChild(githubDiv);
+        gitIcon.setAttribute("title", "check out this repo on my GitHub");
+        gitDiv.appendChild(gitIcon);
+        externalLinksDiv.appendChild(gitDiv);
+
+        // deployDiv + <a> tag
+        const deployDiv = document.createElement("div");
+        deployDiv.setAttribute("class", "deployDiv");
+        const projectIcon = document.createElement("a");
+
+        // get projUrl and if it exists, set <a> attributes
+        // append projIcon to deployDiv
+        let projectUrl = projects[i].projUrl;
+        if (projectUrl) {
+          projectIcon.setAttribute("href", projectUrl);
+          projectIcon.setAttribute("target", "_blank");
+          projectIcon.setAttribute(
+            "class",
+            "fa-solid fa-square-arrow-up-right fa-xl"
+          );
+          projectIcon.setAttribute("title", "open this project");
+          deployDiv.appendChild(projectIcon);
+
+          // append deployDiv to externalLinksDiv
+          externalLinksDiv.appendChild(deployDiv);
+        }
+
+        project.appendChild(externalLinksDiv);
 
         // append project to projects
         projectsContainer.appendChild(project);
